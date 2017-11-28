15df29aec81343c6eff61938f737f09e372b22bc
林田 弘達 <koutatsu.hayashida@hantworks.co.jp>
2017-11-28 19:30:35 +0900

    Viewを大幅追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 Rails.application.routes.draw do
+
   root :to => 'pages#index'
 
+  # 概要表示
+  get '/about' => 'pages#about'
+  get '/dna' => 'pages#dna'
+  get '/rule' => 'pages#rule'
+  get '/forcustomer' => 'pages#forcustomer'
+  get '/forbuyer' => 'pages#forbuyer'
+  get '/formember' => 'pages#formember'
+
+  # ユーザー登録
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  # ユーザー登録プロフィール
   resources :users, only: [:show]
+
+  # その他の処理
+  get '/dashboard/:id' => 'pages#dashboard', as: 'dashboard'
+
+
+  # 利用規約＆ガイドライン表示  
+  get '/shopping' => 'legals#shopping' 
+  get '/terms' => 'legals#terms' 
+  get '/privacy' => 'legals#privacy' 
+  get '/transactions' => 'legals#transactions'
+  get '/team' => 'legals#team' 
+
   
 end
---
+++ test/controllers/legals_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LegalsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
