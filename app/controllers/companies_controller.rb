d045079b5de48bcb726663d1c98da8aa78d496cf
林田 弘達 <koutatsu.hayashida@hantworks.co.jp>
2017-11-29 22:27:34 +0900

    ダッシュボード／店舗情報を追加

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,19 +1,32 @@
 class CompaniesController < ApplicationController
-  def index
+
+  def edit
+    @company = Company.find_by_user_id(current_user.id)
+    unless @company
+      @company = Company.new
+    end
+  end
+
+  def update
+    @company = Company.new(company_params)
+    @company.user_id = current_user.id
+    @company.update_attributes(company_params)
+
+    if @company.save
+      redirect_to :back, notice: "店舗情報を更新しました"
+    else
+      redirect_to :back, notice: "店舗情報を更新できませんでした"
+    end
   end
 
   def show
   end
 
-  def new
+  private
+  def company_params
+    params.require(:company).permit(:store_name, :company_name, :manager_name, 
+      :postal_code, :address, :access, :phone_number, :fax_number, 
+      :business_hours, :description, :homepage)
   end
 
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'companies/index'
-
-  get 'companies/show'
-
-  get 'companies/new'
-
-  get 'companies/create'
-
-  get 'companies/edit'
-
-  get 'companies/update'
-
-  get 'information/new'
-
-  get 'information/create'
-
-  get 'information/edit'
-
-  get 'information/update'
-
-  get 'information/index'
-
-  get 'information/show'
-
   root :to => 'pages#index'
+
+  # ユーザー登録と編集
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
+  resources :users, only: [:show]
+
+  # その他の処理
+  get '/dashboard/:id' => 'pages#dashboard', as: 'dashboard'
+
 
   # 概要表示
   get '/about' => 'pages#about'
@@ -34,13 +18,14 @@ Rails.application.routes.draw do
   get '/forbuyer' => 'pages#forbuyer'
   get '/formember' => 'pages#formember'
 
-  # ユーザー登録
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
-  resources :users, only: [:show]
+  # 店舗情報
+  get 'companies/edit'
+  get 'companies/show'
+  post 'companies/update'
 
-  # その他の処理
-  get '/dashboard/:id' => 'pages#dashboard', as: 'dashboard'
-
+  # お知らせ表示
+  get 'information/index'
+  get 'information/show'
 
   # 利用規約＆ガイドライン表示  
   get '/shopping' => 'legals#shopping' 
@@ -48,6 +33,8 @@ Rails.application.routes.draw do
   get '/privacy' => 'legals#privacy' 
   get '/transactions' => 'legals#transactions'
   get '/team' => 'legals#team' 
+
+
 
   
 end
