957027f00924da70f3fdf9ad397a5b4e9b29ad4f
林田 弘達 <koutatsu.hayashida@hantworks.co.jp>
2017-11-30 00:07:52 +0900

    店舗情報に画像をを追加

---
+++ db/migrate/20171129150656_add_image_columns_to_companies.rb
@@ -0,0 +1,5 @@
+class AddImageColumnsToCompanies < ActiveRecord::Migration[5.0]
+  def change
+    add_column :companies, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129095017) do
+ActiveRecord::Schema.define(version: 20171129150656) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "store_name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171129095017) do
     t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "image"
   end
 
   create_table "information", force: :cascade do |t|
